bc9b81c83df1fc3dacae477c77e855d40ee38880
Sergio B <sbarajas510@gmail.com>
2019-09-25 16:55:44 -0700

    added my phrases

--- js/Game.js
+++ js/Game.js
@@ -4,8 +4,21 @@
 
  class Game{
      constructor(){
-         missed : 0;
-         phrases:[];
-         activePhrase: 'null';
+         this.missed = 0;
+         this.phrases = this.createPhrases();
+         this.activePhrase = 'null';
+     }
+     /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game */
+     createPhrases(){
+        return [
+            new Phrase("Breathe Through the Pain, It Always Gets Easier with Time"), 
+            new Phrase("Bring Home The Bacon"),
+            new Phrase("Bread always falls buttered side down"),
+            new Phrase("Better to have loved and lost than never to have loved at all"),
+            new Phrase("Absolute power corrupts absolutely"),
+            
+          ]
      }
  };
--- js/app.js
+++ js/app.js
@@ -7,7 +7,10 @@
 
 // const phrase = new Phrase('Life is like a box of chocolates');
 // console.log(`Phrase - phrase: ${phrase.phrase}`);
-
+const game = new Game();
+game.phrases.forEach((phrase, index) => {
+  console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
 
 
 document.getElementById("btn__reset").addEventListener('click', () => {});
