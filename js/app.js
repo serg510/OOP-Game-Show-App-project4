02523df1a7065d42226817f0929a037e8dddd51e
Sergio B <sbarajas510@gmail.com>
2019-09-25 15:51:18 -0700

    made some changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// const phrase = new Phrase(console.log('hello phrase'));
+// const game = new Game(console.log('hello game'));
+
+// const phrase = new Phrase('Life is like a box of chocolates');
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+
+
+document.getElementById("btn__reset").addEventListener('click', () => {});
+document.getElementById("qwerty").addEventListener('click', () => {});
